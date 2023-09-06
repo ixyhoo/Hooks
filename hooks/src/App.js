6370ca57e93561b1c1394cf494a3aad016fb7d09
ixyhoo <ixyhoo@outlook.fr>
2023-09-06 14:45:36 +0200

    I learn useRef

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const App = () => {
   const [myNumber, setMyNumber] = useState();
-
+  const selectedNumber = useRef();
   useEffect(() => {
   console.log("number is change");
   }, [myNumber]);
@@ -10,7 +10,13 @@ const App = () => {
     <div className="app">
       <h1>FLY</h1>
       <p>{myNumber}</p>
-      <button onClick={() => setMyNumber(myNumber + 1)}>incrementer + 1</button>
+      <button onClick={() => setMyNumber(Number(myNumber) + 1)}>incrementer + 1</button>
+      <label htmlFor="">Number beguin</label>
+      <input 
+      type="number" 
+      defaultValue={0} 
+      ref={selectedNumber}
+      onChange={(e) => setMyNumber(selectedNumber.current.value)}/>
     </div>
   );
 };
