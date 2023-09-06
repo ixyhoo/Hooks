8f3b45cffc78e1f6b24e9f43cc820b18fa14f058
ixyhoo <ixyhoo@outlook.fr>
2023-09-06 14:35:51 +0200

    I learn useState and UseEffect

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+  const [myNumber, setMyNumber] = useState();
+
+  useEffect(() => {
+  console.log("number is change");
+  }, [myNumber]);
   return (
-    <div>
-      
+    <div className="app">
+      <h1>FLY</h1>
+      <p>{myNumber}</p>
+      <button onClick={() => setMyNumber(myNumber + 1)}>incrementer + 1</button>
     </div>
   );
 };
