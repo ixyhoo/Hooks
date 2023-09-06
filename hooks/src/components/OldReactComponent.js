578f680272683c0f69040ab5d06d867e6c0ce8f6
ixyhoo <ixyhoo@outlook.fr>
2023-09-06 14:22:44 +0200

    I start hooks

--- hooks/src/components/OldReactComponent.js
+++ hooks/src/components/OldReactComponent.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class OldReactComponent extends Component {
+    constructor(props) {
+    super(props);
+this.state = {
+counter:0,
+age: 23,
+name: "denis"
+}
+this.handleClick = this.handleClick.bind(this);
+}
+handleClick() {
+}
+
+componentDidMount() {
+}
     render() {
         return (
             <div>
